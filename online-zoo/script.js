c415910c80b5e5f90087ef1770413153869eab12
vhswry <vasvasa478@gmail.com>
2022-10-14 01:26:50 +0300

    feat: add popup for Testimonials

--- online-zoo/script.js
+++ online-zoo/script.js
@@ -1,26 +1,87 @@
-window.addEventListener('load', function(e){
+window.addEventListener('load', function (e) {
     let menuInput = document.getElementById("nav-enabler"),
         main = document.body.getElementsByTagName('main')[0];
-        let disableScroll = () => {
-            document.body.style.overflowY = 'hidden';
-            main.style.filter = 'brightness(0.5)';
-            menuInput.checked = true;
-        };
-        let enableScroll = () => {
-            document.body.style.overflowY = '';
-            main.style.filter = '';
+    let disableScroll = () => {
+        document.body.style.overflowY = 'hidden';
+    };
+    let enableScroll = () => {
+        document.body.style.overflowY = '';
+    };
+
+    menuInput.addEventListener('change', function (e) {
+        e.target.checked
+            ? (disableScroll(), main.style.filter = 'brightness(0.5)')
+            : (enableScroll(), main.style.filter = '');
+    })
+
+    main.addEventListener('click', function (e) {
+        if (document.body.style.overflowY == 'hidden') {
             menuInput.checked = false;
-        };
-
-    menuInput.addEventListener('change', function(e){
-        e.target.checked 
-        ? disableScroll() 
-        : enableScroll();
+        } 
     })
-    main.addEventListener('click',function(e){
-        if(document.body.style.overflowY == 'hidden'){
-            enableScroll();
+
+    let popupContainer = this.document.createElement('div');
+    popupContainer.classList.add('popupContainer');
+
+    this.document.body.appendChild(popupContainer);
+
+    let isPopupEnabled = false;
+    let enablePopUp = () => {
+        popupContainer.classList.add('active');
+        isPopupEnabled = true;
+        // disableScroll();
+
+    },
+    disablePopUp = () => {
+        popupContainer.classList.remove('active');
+        // enableScroll();
+        isPopupEnabled = false;
+    };
+
+    popupContainer.addEventListener('click', function(e){
+        if(e.target === popupContainer){
+            disablePopUp();
+        } 
+    });
+
+ 
+
+    //////
+    
+    document.addEventListener('wheel', function(e) {
+        if(isPopupEnabled){
+            e.preventDefault();
         }
-    })
+        // doStuff(e);
+    }, { passive: false });
+    
+    //////
 
+
+
+
+
+    let assignPopupContent = (contentNode) => {
+        popupContainer.innerHTML = '';
+        popupContainer.appendChild(contentNode);
+    }
+
+    // assignPopupContent(document.createTextNode('assignPopupContent').cloneNode(true))
+    // let test = document.querySelector('.user-cards-container .gradient');
+    // assignPopupContent(test.cloneNode(true))
+    // enablePopUp();
+
+    let testimonials = document.querySelector('div.user-cards-container');
+
+    Array.from(testimonials.childNodes).filter((el) => el.classList)
+    .map((el)=>{
+        el.addEventListener('click', (e) => {
+            assignPopupContent(el.cloneNode(true));
+            enablePopUp();
+        })
+    })
+    // testimonials.addEventListener('click', function(e){
+    //     if(e.target)
+    // })
+    
 })
